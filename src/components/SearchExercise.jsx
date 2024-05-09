182eaea50e4cfcf9c06f9beb5ae2cbdc24826403
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-09 19:41:20 +0700

    starting Exercise page

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Box, Button, Stack, Typography, TextField} from '@mui/material'
 
 const SearchExercise = () => {
   return (
-    <div>SearchExercise</div>
+    <Stack alignItems="center"
+    mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize: {
+        lg: '44px' ,
+        xs: '30px'
+      }}}
+      mb="50px"
+      textAlign="center">
+        Awesome Exercises You
+        <br />
+        Should know
+      </Typography>
+      <Box position="relative"
+      mb="72px">
+        <TextField 
+        sx={{
+          input: {
+            fontWeight: '700',border: 'none', borderRadius: '4px'
+            }}}
+        height = "76px"
+        value=""
+        onChange={(e) => {}}
+        placeholder="Search Exercises"
+        type="text"/>
+      </Box>
+    </Stack>
   )
 }
 
